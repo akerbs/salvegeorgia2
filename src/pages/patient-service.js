8264d8b7655fcff9fad7da3d8797f50554d7d82a
Anatol Kerbs <anker2702@gmail.com>
2020-11-25 16:25:39 +0300

    251120_3

--- src/pages/patient-service.js
+++ src/pages/patient-service.js
@@ -4,6 +4,8 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Layout from "../components/layout"
+
 import PatientServicePageContent from "../components/patientServicePageContent"
 
 const window = require("global/window")
@@ -23,13 +25,13 @@ export default function () {
   }, [])
 
   return (
-    <>
+    <Layout>
       <div className={classes.root}>
         <SEO title="About us" />
         <Header />
         <PatientServicePageContent />
         <Footer />
       </div>
-    </>
+    </Layout>
   )
 }
