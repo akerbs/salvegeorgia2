1da5f57b1a2c90083359f772bc53d3a9b4a947a2
Anatol Kerbs <anker2702@gmail.com>
2020-11-27 16:32:17 +0300

    271120_11

--- src/components/complexButtons.js
+++ src/components/complexButtons.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react"
-import { makeStyles, useTheme } from "@material-ui/core/styles"
+import { useTheme } from "@material-ui/core/styles"
 import ButtonBase from "@material-ui/core/ButtonBase"
 import Typography from "@material-ui/core/Typography"
 import { LanguageContext } from "../components/layout"
@@ -42,75 +42,6 @@ const images = [
     width: "50%",
   },
 ]
-
-const useStyles = makeStyles(theme => ({
-  // image: {
-  //   position: "relative",
-  //   height: "50vh", //330
-  //   [theme.breakpoints.down("xs")]: {
-  //     width: "100% !important", // Overrides inline-style
-  //     height: "20vh", // 105
-  //   },
-  //   "&:hover, &$focusVisible": {
-  //     zIndex: 1,
-  //     "& $imageBackdrop": {
-  //       opacity: 0.15,
-  //     },
-  //     "& $imageMarked": {
-  //       opacity: 0,
-  //     },
-  //     "& $imageTitle": {
-  //       border: "4px solid currentColor",
-  //     },
-  //   },
-  // },
-  // focusVisible: {},
-  // imageButton: {
-  //   position: "absolute",
-  //   left: 0,
-  //   right: 0,
-  //   top: 0,
-  //   bottom: 0,
-  //   display: "flex",
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  //   color: theme.palette.common.white,
-  // },
-  // imageSrc: {
-  //   position: "absolute",
-  //   left: 0,
-  //   right: 0,
-  //   top: 0,
-  //   bottom: 0,
-  //   backgroundSize: "cover",
-  //   backgroundPosition: "center 40%",
-  // },
-  // imageBackdrop: {
-  //   position: "absolute",
-  //   left: 0,
-  //   right: 0,
-  //   top: 0,
-  //   bottom: 0,
-  //   backgroundColor: theme.palette.common.black,
-  //   opacity: 0.4,
-  //   transition: theme.transitions.create("opacity"),
-  // },
-  // imageTitle: {
-  //   position: "relative",
-  //   padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${
-  //     theme.spacing(1) + 6
-  //   }px`,
-  // },
-  // imageMarked: {
-  //   // height: 3,
-  //   width: 18,
-  //   backgroundColor: theme.palette.common.white,
-  //   position: "absolute",
-  //   bottom: -2,
-  //   left: "calc(50% - 9px)",
-  //   transition: theme.transitions.create("opacity"),
-  // },
-}))
 
 export default function ComplexButtons() {
   const theme = useTheme()
