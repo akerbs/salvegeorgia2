0189d80e0d70489770d426eadf897d336fe460c4
Anatol Kerbs <anker2702@gmail.com>
2020-11-27 16:47:39 +0300

    271120_16

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import SEO from "../components/seo"
@@ -6,6 +6,19 @@ import IndexPageContent from "../components/indexPageContent"
 const window = require("global/window")
 
 export default function IndexPage() {
+  const [showAfterLoading, setShowAfterLoading] = useState(false)
+
+  function startShowAfterLoading() {
+    setShowAfterLoading(true)
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      startShowAfterLoading()
+    }, 300)
+    return () => clearTimeout(timer)
+  }, [])
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
@@ -14,8 +27,8 @@ export default function IndexPage() {
     <div style={{ minHeight: "100vh", padding: 0 }}>
       <SEO title="Home" />
       <Header />
-      <IndexPageContent />
-      <Footer />
+      {showAfterLoading && <IndexPageContent />}
+      {showAfterLoading && <Footer />}
     </div>
   )
 }
