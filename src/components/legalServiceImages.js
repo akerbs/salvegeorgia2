12b970df3fe0a7a9f25faeaa78f4bb099fd4fdb7
Anatol Kerbs <anker2702@gmail.com>
2020-12-16 13:31:07 +0300

    161220_9

--- src/components/legalServiceImages.js
+++ src/components/legalServiceImages.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useTheme } from "@material-ui/core/styles"
 import GridList from "@material-ui/core/GridList"
 import GridListTile from "@material-ui/core/GridListTile"
@@ -15,6 +15,7 @@ import image5m from "../images/legal_service/5m.jpg"
 import image6 from "../images/legal_service/6.jpg"
 import image6m from "../images/legal_service/6m.jpg"
 import image7 from "../images/legal_service/7.jpg"
+import CircularProgress from "@material-ui/core/CircularProgress"
 
 const window = require("global/window")
 
@@ -98,8 +99,32 @@ const tileDataMin = [
   },
 ]
 
+const IMAGES = window.innerWidth >= 600 ? tileDataMax : tileDataMin
+
 export default function () {
   const theme = useTheme()
+
+  const [imgsLoaded, setImgsLoaded] = useState(false)
+
+  useEffect(() => {
+    const loadImage = tile => {
+      return new Promise((resolve, reject) => {
+        const loadImg = new Image()
+        loadImg.src = tile.img
+        // wait 2 seconds to simulate loading time
+        loadImg.onload = () =>
+          setTimeout(() => {
+            resolve(tile.img)
+          }, 1)
+
+        loadImg.onerror = err => reject(err)
+      })
+    }
+
+    Promise.all(IMAGES.map(image => loadImage(image)))
+      .then(() => setImgsLoaded(true))
+      .catch(err => console.log("Failed to load images", err))
+  }, [])
 
   return (
     <div
@@ -121,8 +146,8 @@ export default function () {
         cols={3}
         spacing={2}
       >
-        {window.innerWidth >= 600 &&
-          tileDataMax.map(tile => (
+        {imgsLoaded ? (
+          IMAGES.map(tile => (
             <GridListTile
               key={tile.img}
               cols={tile.cols || 1}
@@ -130,17 +155,21 @@ export default function () {
             >
               <img src={tile.img} alt={tile.title} />
             </GridListTile>
-          ))}
-        {window.innerWidth <= 599 &&
-          tileDataMin.map(tile => (
-            <GridListTile
-              key={tile.img}
-              cols={tile.cols || 1}
-              style={{ padding: 1, margin: 0 }}
-            >
-              <img src={tile.img} alt={tile.title} />
-            </GridListTile>
-          ))}
+          ))
+        ) : (
+          <div
+            style={{
+              displax: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              textAlign: "center",
+            }}
+          >
+            <CircularProgress
+            // color="secondary"
+            />
+          </div>
+        )}
       </GridList>
     </div>
   )
